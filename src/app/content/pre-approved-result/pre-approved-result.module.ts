deb321ed1143224ae063d5984e10a498dbb3c525
denishm270991 <denishm270991@gmail.com>
2022-03-21 15:56:04 -0400

    maquetadas las vistas pre-approved-result en sus dos versiones

--- src/app/content/pre-approved-result/pre-approved-result.module.ts
+++ src/app/content/pre-approved-result/pre-approved-result.module.ts
@@ -8,12 +8,25 @@ import { PreApprovedResultPageRoutingModule } from './pre-approved-result-routin
 
 import { PreApprovedResultPage } from './pre-approved-result.page';
 
+import {  HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    PreApprovedResultPageRoutingModule
+    PreApprovedResultPageRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [PreApprovedResultPage]
 })
--- src/app/content/pre-approved-result/pre-approved-result.page.ts
+++ src/app/content/pre-approved-result/pre-approved-result.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-pre-approved-result',
   templateUrl: './pre-approved-result.page.html',
@@ -7,17 +8,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PreApprovedResultPage implements OnInit {
 
-  result: boolean;
+  result: string;
+  language: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private translate: TranslateService,
   ) {
     this.activatedRoute.params.subscribe(params => {
       this.result = params.result;
     });
+    this.language = 'en';
+    translate.setDefaultLang('en');
   }
 
   ngOnInit() {
+  }
+
+  onSelectChange(selectedValue: any) {
+    console.log('enter here');
+    this.translate.setDefaultLang(selectedValue.detail.value);
   }
 
 }
