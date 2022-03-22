bf8f276bd8faa44fd6c0bee89cdcafc66c6bf389
denishm270991 <denishm270991@gmail.com>
2022-03-22 03:12:43 -0400

    todas las vistas están maquetadas

--- src/app/content/close-your-new-home/close-your-new-home.module.ts
+++ src/app/content/close-your-new-home/close-your-new-home.module.ts
@@ -7,13 +7,25 @@ import { IonicModule } from '@ionic/angular';
 import { CloseYourNewHomePageRoutingModule } from './close-your-new-home-routing.module';
 
 import { CloseYourNewHomePage } from './close-your-new-home.page';
-
+import {  HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CloseYourNewHomePageRoutingModule
+    CloseYourNewHomePageRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [CloseYourNewHomePage]
 })
--- src/app/content/close-your-new-home/close-your-new-home.page.ts
+++ src/app/content/close-your-new-home/close-your-new-home.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-close-your-new-home',
   templateUrl: './close-your-new-home.page.html',
   styleUrls: ['./close-your-new-home.page.scss'],
 })
 export class CloseYourNewHomePage implements OnInit {
+  language: string;
 
-  constructor() { }
+  constructor(private translate: TranslateService) {
+    this.language = "en";
+    translate.setDefaultLang('en');
+  }
 
   ngOnInit() {
   }
 
+  onSelectChange(selectedValue: any) {
+    this.translate.setDefaultLang(selectedValue.detail.value);
+  }
 }
