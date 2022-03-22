16ba530e703540942f5e37df72f36940df3b767e
denishm270991 <denishm270991@gmail.com>
2022-03-22 01:59:19 -0400

    maquetación de la vista hire-real-state-agent con el servicio translate

--- src/app/content/hire-real-state-agent/hire-real-state-agent.module.ts
+++ src/app/content/hire-real-state-agent/hire-real-state-agent.module.ts
@@ -7,13 +7,26 @@ import { IonicModule } from '@ionic/angular';
 import { HireRealStateAgentPageRoutingModule } from './hire-real-state-agent-routing.module';
 
 import { HireRealStateAgentPage } from './hire-real-state-agent.page';
+import {  HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HireRealStateAgentPageRoutingModule
+    HireRealStateAgentPageRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [HireRealStateAgentPage]
 })
--- src/app/content/hire-real-state-agent/hire-real-state-agent.page.ts
+++ src/app/content/hire-real-state-agent/hire-real-state-agent.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-hire-real-state-agent',
   templateUrl: './hire-real-state-agent.page.html',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HireRealStateAgentPage implements OnInit {
 
-  constructor() { }
+  language: string;
+    
+  constructor(private translate: TranslateService) {
+    this.language = "en";
+    translate.setDefaultLang('en');
+  }
 
   ngOnInit() {
   }
 
+  onSelectChange(selectedValue: any) {
+    this.translate.setDefaultLang(selectedValue.detail.value);
+  }
 }
