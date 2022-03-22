5d125aebf9b785115d239d4eaa8eff59e8549a2e
asdrubal9104 <asdrubalsanchezgarcia@gmail.com>
2022-03-22 18:40:32 -0400

    probando

--- src/app/content/signup/signup.module.ts
+++ src/app/content/signup/signup.module.ts
@@ -12,8 +12,8 @@ import {  HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
-}
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+};
 
 @NgModule({
   imports: [
