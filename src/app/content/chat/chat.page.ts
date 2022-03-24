fe865a41f439ab5caac4aee68dc3f3ff301abb48
denishm270991 <denishm270991@gmail.com>
2022-03-23 22:18:47 -0400

    corregido lo de las rutas que no tuvieran doble comillas y que no incluyeran src

--- src/app/content/chat/chat.page.ts
+++ src/app/content/chat/chat.page.ts
@@ -5,17 +5,17 @@ import { IonContent, Platform } from '@ionic/angular';
 // import * as firebase from 'firebase/app';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database'
-import { MessageI } from 'src/app/interfaces/message';
+import { MessageI } from '../../interfaces/message';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDE3IkmgIdxh_NDWuFf78jR5GRv2eelad4",
+  apiKey: 'AIzaSyDE3IkmgIdxh_NDWuFf78jR5GRv2eelad4',
   authDomain: "hba2022-e5821.firebaseapp.com",
-  databaseURL: "https://hba2022-e5821-default-rtdb.firebaseio.com",
-  projectId: "hba2022-e5821",
-  storageBucket: "hba2022-e5821.appspot.com",
-  messagingSenderId: "239569286794",
-  appId: "1:239569286794:web:0966c7e3330cdcd5c01f73",
-  measurementId: "G-J6ZLJ2MNQ6"
+  databaseURL: 'https://hba2022-e5821-default-rtdb.firebaseio.com',
+  projectId: 'hba2022-e5821',
+  storageBucket: 'hba2022-e5821.appspot.com',
+  messagingSenderId: '239569286794',
+  appId: '1:239569286794:web:0966c7e3330cdcd5c01f73',
+  measurementId: 'G-J6ZLJ2MNQ6'
 };
 
 @Component({
@@ -27,7 +27,7 @@ const firebaseConfig = {
 export class ChatPage implements OnInit {
 
   // @ViewChild('content') content: Content;
-  @ViewChild("scrollElement") content: IonContent;
+  @ViewChild('scrollElement') content: IonContent;
   user = {
     uid: 'eMWXZ083bBYgEJrM1cdC7c5K6I32',
     displayName: 'Denis Hernández Michel'
--- src/app/content/shop-your-home/shop-your-home.module.ts
+++ src/app/content/shop-your-home/shop-your-home.module.ts
@@ -11,7 +11,7 @@ import {  HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
   imports: [
